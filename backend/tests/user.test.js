e28722ce2100703ecd100fba1ae0f2f0be199371
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-11-04 23:32:28 +0500

    valid credentials

--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -7,10 +7,25 @@ describe("users", () => {
             it("get top seller",async () => {
                 const topSeller = "Basir";
                 await request(app)
-                .get("/api/top-sellers")
+                .get("/api/users/top-sellers")
                 .expect(200)
                 .then((response) => {
                     expect(response.body[0].name).toBe(topSeller)
+                });
+            });
+        });
+    });
+
+    describe("login", () => {
+        describe("given valid email and password, user must be logged in", () => {
+            it("valid credentials",async () => {
+                const email = "admin@example.com"
+                const password = "1234"
+                await request(app)
+                .get("/api/users/signin")
+                .expect(200)
+                .then((response) => {
+                    expect(response.body.email).toBe(email)
                 });
             });
         });
