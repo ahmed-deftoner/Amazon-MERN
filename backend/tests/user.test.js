7dd8cb34bc67dd86a01cc4c15c3557852af700fc
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-11-04 23:37:45 +0500

    invalid password

--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -19,13 +19,46 @@ describe("users", () => {
     describe("login", () => {
         describe("given valid email and password, user must be logged in", () => {
             it("valid credentials",async () => {
-                const email = "admin@example.com"
-                const password = "1234"
+                const data = {
+                    email : "admin@example.com",
+                    password : "1234"
+                }
                 await request(app)
-                .get("/api/users/signin")
+                .post("/api/users/signin")
+                .send(data)
                 .expect(200)
                 .then((response) => {
-                    expect(response.body.email).toBe(email)
+                    expect(response.body.email).toBe(data.email)
+                });
+            });
+
+            it("invalid password",async () => {
+                const data = {
+                    email : "admin@example.com",
+                    password : "1238"
+                }
+                const msg = "Invalid email or password'" 
+                await request(app)
+                .post("/api/users/signin")
+                .send(data)
+                .expect(401)
+                .then((response) => {
+                    expect(response.body.message).toBe(msg)
+                });
+            });
+
+            it("valid credentials",async () => {
+                const data = {
+                    email : "admn@example.com",
+                    password : "1234"
+                }
+                const msg = "Invalid email or password'" 
+                await request(app)
+                .post("/api/users/signin")
+                .send(data)
+                .expect(401)
+                .then((response) => {
+                    expect(response.body.message).toBe(msg)
                 });
             });
         });
