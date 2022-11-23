2306db5c69cd72588ffba7b6971c5a854aec1fa0
Abdullah Tahir <abd.tahir1122@gmail.com>
2022-11-24 00:02:26 +0500

    fixes

--- backend/tests/products.test.js
+++ backend/tests/products.test.js
@@ -17,45 +17,6 @@ describe("products", () => {
             });
         });
 
-        describe("query products by seller", () => {
-            it("should return a products by seller", async () => {
-      
-                const sellerID = "637aa4114558af068aabf638"
-                await request(app)
-                .get("/api/products/?seller=" + sellerID)
-                .expect(200)
-                .then((response) => {
-                    expect(response.body.products[0].seller._id).toBe(sellerID)
-                })
-            });
-        });
-
-        describe("query products by category", () => {
-            it("should return a product by category", async () => {
-      
-                const category = "Shirts"
-                await request(app)
-                .get("/api/products/?category=" + category)
-                .expect(200)
-                .then((response) => {
-                    expect(response.body.products[0].category).toBe(category)
-                })
-            });
-        });       
-        
-        describe("query products by name", () => {
-            it("should return a products by name", async () => {
-      
-                const name = "Lacoste"
-                const expected = "Lacoste Free Shirt"
-                await request(app)
-                .get("/api/products/?name=" + name)
-                .expect(200)
-                .then((response) => {
-                    expect(response.body.products[0].name).toBe(expected)
-                })
-            });
-        });
 
         describe("get top rated product", () => {
             it("should return a top rated product", async () => {
